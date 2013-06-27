8f699d805e2f42cd62385c77104034753335f78c
Tom Li <tom@qvivo.com>
2013-06-27 10:42:12 +0800

    - fix ios build

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
+make PLATFORM=IOS
 
-make clean
-make library
-make clean
-make IOS=1 library
-make clean
-make IOS=2 library
-make lipo
-
+cp -a include/leveldb $QVIVO_SDK/iphonesimulator/include/
+cp -a include/leveldb $QVIVO_SDK/iphoneos/include/
 cp libleveldb.a $QVIVO_SDK/iphonesimulator/lib/
 cp libleveldb.a $QVIVO_SDK/iphoneos/lib/
+
+echo "Done."
